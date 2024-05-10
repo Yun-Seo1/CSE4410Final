32374766430b47ae349dc32607e1070cef4ddf28
Luigiguy25 <163463512+Luigiguy25@users.noreply.github.com>
2024-05-09 18:49:52 -0700

    Add files via upload

---
+++ Assets/Scripts/FalsePlatform.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Timers;
+using UnityEngine;
+
+public class FalsePlatform : MonoBehaviour{
+    bool isFalling = false;
+    float fallSpeed = 0;
+
+    // Update is called once per frame
+    void Update(){
+        if(isFalling){
+            fallSpeed += Time.deltaTime / 10;
+            transform.position = new Vector3(transform.position.x, transform.position.y - fallSpeed, transform.position.z);
+        }
+    }
+    void OnTriggerEnter(Collider collider){
+        if(collider.gameObject.name == "player"){
+            isFalling = true;
+        }
+    }
+
+}
